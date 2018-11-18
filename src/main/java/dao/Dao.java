634c66c2d9d34b20caa2ffab13767966ea0fdf0f
CicadasOutsideMyWindow <rrondonbombomm.kakuta@gmail.com>
2018-11-18 10:23:33 -0800

    add a method; change signature

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -1,11 +1,13 @@
 package dao;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 
 public interface Dao<T,S> {
 
     void create(S s);
+    DBObject retrieve(String id);
     void update(BasicDBObject query, BasicDBObject updates);
-    void delete(S s);
+    void delete(String id);
 
 }
--- src/main/java/dao/ParticipantDao.java
+++ src/main/java/dao/ParticipantDao.java
@@ -3,6 +3,8 @@ package dao;
 import adaptors.ParticipantAdaptor;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import model.Participant;
 
 public class ParticipantDao implements Dao<DBCollection,Participant> {
@@ -17,13 +19,23 @@ public class ParticipantDao implements Dao<DBCollection,Participant> {
         collection.insert(ParticipantAdaptor.toDBObject(participant));
     }
 
+    public DBObject retrieve(String id) {
+        BasicDBObject _id = new BasicDBObject();
+        DBCursor cursor = collection.find(_id);
+        _id.put("_id", id);
+
+        return cursor.next();
+    }
+
     public void update(BasicDBObject query, BasicDBObject updates) {
         BasicDBObject u = new BasicDBObject();
         u.put("$set", updates);
         collection.update(query, u);
     }
 
-    public void delete(Participant participant) {
-        collection.remove(ParticipantAdaptor.toDBObject(participant));
+    public void delete(String id) {
+        BasicDBObject _id = new BasicDBObject();
+        _id.put("_id", id);
+        collection.remove(_id);
     }
 }
