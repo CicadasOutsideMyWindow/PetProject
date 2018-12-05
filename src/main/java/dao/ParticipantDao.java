fe3569c015649fba6d314df13d1a6cdc880f53e9
CicadasOutsideMyWindow <rrondonbombomm.kakuta@gmail.com>
2018-12-04 22:18:54 -0800

    Participant DAO reworked

--- src/main/java/dao/ParticipantDao.java
+++ src/main/java/dao/ParticipantDao.java
@@ -1,47 +1,64 @@
 package dao;
 
 import com.google.gson.Gson;
-import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.util.JSON;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import model.Participant;
+import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class ParticipantDao implements Dao<DBCollection,Participant> {
 
-    DBCollection collection;
+    MongoCollection collection;
 
-    public ParticipantDao(DBCollection collection) {
+    public ParticipantDao(MongoCollection collection) {
         this.collection = collection;
     }
 
-    public void create(Participant participant) {
+    public Document create(Participant participant) {
 
         Gson gson = new Gson();
-        String json = gson.toJson(participant);
-        DBObject participantDBObject = (DBObject) JSON.parse(json);
+        Document p = Document.parse(gson.toJson(participant));
 
-        collection.insert(participantDBObject);
+        collection.insertOne(p);
+
+        return p;
     }
 
-    public DBObject retrieve(String id) {
-        BasicDBObject _id = new BasicDBObject();
+    public Document retrieve(String id) {
 
-        DBObject participant = collection.findOne(_id);
-        _id.put("_id", id);
+        Document p  = (Document) collection.find(eq("_id", id)).first();
 
-        return participant;
+        return p;
     }
 
-    public void update(BasicDBObject query, BasicDBObject updates) {
-        BasicDBObject u = new BasicDBObject();
-        u.put("$set", updates);
-        collection.update(query, u);
+    public Document update(Participant participant) {
+
+        Gson gson = new Gson();
+        Document p = Document.parse(gson.toJson(participant));
+
+        collection.replaceOne(Filters.eq("_id", participant.getId()), p);
+
+        return p;
+
     }
 
-    public void delete(String id) {
-        BasicDBObject _id = new BasicDBObject();
-        _id.put("_id", id);
-        collection.remove(_id);
+    public Document delete(String id) {
+
+        Gson gson = new Gson();
+
+        Document p = retrieve(id);
+
+        Participant participant = gson.fromJson(gson.toJson(p), Participant.class);
+        participant.setDeleted();
+
+        update(participant);
+
+        p = Document.parse(gson.toJson(participant));
+
+        return p;
+
     }
 }
