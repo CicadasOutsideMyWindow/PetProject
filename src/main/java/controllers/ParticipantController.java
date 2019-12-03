f8ecbcee4e2e5209edd055d88e55a806d503187f
CicadasOutsideMyWindow <rrondonbombomm.kakuta@gmail.com>
2019-12-02 20:06:24 -0800

    working on the ParticipantController

--- src/main/java/controllers/ParticipantController.java
+++ src/main/java/controllers/ParticipantController.java
@@ -1,4 +1,15 @@
 package controllers;
 
+import dao.ParticipantDao;
+
 public class ParticipantController {
+
+    private ParticipantDao participantDao;
+
+    public ParticipantController (ParticipantDao participantDao) {
+        this.participantDao = participantDao;
+    }
+
+//    public String get(HttpServletRequest, StrPar)
+
 }
