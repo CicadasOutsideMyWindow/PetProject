d8539cd56c6c21b18da0b5bae7fa25e978b22111
CicadasOutsideMyWindow <rrondonbombomm.kakuta@gmail.com>
2019-12-02 20:04:56 -0800

    working on the EventAdaptor

--- src/main/java/adaptors/EventAdaptor.java
+++ src/main/java/adaptors/EventAdaptor.java
@@ -1,4 +1,22 @@
 package adaptors;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import model.Event;
+
 public class EventAdaptor {
+
+    public static final DBObject toDBObject(Event event) {
+        return new BasicDBObject(
+                "_id", event.getId())
+                .append("eventName", event.getTitle())
+                .append("startDate", event.getStartDate())
+                .append("duration", event.getDuration())
+                .append("location", event.getLocation())
+                .append("description", event.getDescription())
+                .append("participants", event.getParticipants())
+                .append("waitlist", event.getWaitlist())
+                .append("eventHost", event.getEventHost());
+    }
+
 }
