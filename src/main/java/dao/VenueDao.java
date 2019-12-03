77e70f4933a08a0db517c736a727cd421c5aea52
CicadasOutsideMyWindow <rrondonbombomm.kakuta@gmail.com>
2019-12-02 20:07:06 -0800

    working on the VenueDao

--- src/main/java/dao/VenueDao.java
+++ src/main/java/dao/VenueDao.java
@@ -1,4 +1,42 @@
 package dao;
 
+import adaptors.VenueAdaptor;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import model.Venue;
+
 public class VenueDao {
-}
+
+    DBCollection collection;
+
+    public VenueDao(DBCollection collection) {
+            this.collection = collection;
+        }
+
+        public void create(Venue venue) {
+            collection.insert(VenueAdaptor.toDBObject(venue));
+        }
+
+        public DBObject retrieve(String id) {
+            BasicDBObject _id = new BasicDBObject();
+            DBCursor cursor = collection.find(_id);
+            _id.put("_id", id);
+
+            return cursor.next();
+        }
+
+        public void update(BasicDBObject query, BasicDBObject updates) {
+            BasicDBObject u = new BasicDBObject();
+            u.put("$set", updates);
+            collection.update(query, u);
+        }
+
+        public void delete(String id) {
+            BasicDBObject _id = new BasicDBObject();
+            _id.put("_id", id);
+            collection.remove(_id);
+        }
+
+    }
